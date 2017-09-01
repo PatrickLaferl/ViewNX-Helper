d0436bf50f689bc23bef528766c604c4cd547477
betacar0tin <vert@live.de>
2017-09-01 16:54:32 +0200

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ViewNxHelper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.taskBarMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.startViewNXToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.taskBarMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.taskBarMenu;
+            this.notifyIcon.Text = "ViewNX Helper";
+            this.notifyIcon.Visible = true;
+            // 
+            // taskBarMenu
+            // 
+            this.taskBarMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startViewNXToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.taskBarMenu.Name = "taskBarMenu";
+            this.taskBarMenu.Size = new System.Drawing.Size(153, 70);
+            // 
+            // startViewNXToolStripMenuItem
+            // 
+            this.startViewNXToolStripMenuItem.Name = "startViewNXToolStripMenuItem";
+            this.startViewNXToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.startViewNXToolStripMenuItem.Text = "Start ViewNX";
+            this.startViewNXToolStripMenuItem.Click += new System.EventHandler(this.startViewNXToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(70, 70);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(70, 70);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(70, 70);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "ViewNX Helper";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.taskBarMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip taskBarMenu;
+        private System.Windows.Forms.ToolStripMenuItem startViewNXToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,187 @@
+﻿namespace ViewNxHelper
+{
+    using System;
+    using System.Diagnostics;
+    using System.Drawing;
+    using System.Runtime.InteropServices;
+    using System.Windows.Forms;
+
+    public partial class MainForm : Form
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;        // x position of upper-left corner
+            public int Top;         // y position of upper-left corner
+            public int Right;       // x position of lower-right corner
+            public int Bottom;      // y position of lower-right corner
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool GetWindowRect(IntPtr hwnd, out RECT lpRect);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        enum State
+        {
+            Inactive,
+            Ready,
+            Busy,
+        }
+
+        private Timer startTimer;
+        private Timer updateTimer;
+        private PerformanceCounter processCpu;
+        private const string procName = "ViewNX2";
+        private const string procPath = @"C:\Program Files\Nikon\ViewNX 2\ViewNX 2\ViewNX2.exe";
+
+        public MainForm()
+        {
+            this.InitializeComponent();
+
+            this.updateTimer = new Timer();
+            this.updateTimer.Interval = 1000;
+            this.updateTimer.Tick += this.UpdateTimer_Tick;
+
+            this.startTimer = new Timer();
+            this.startTimer.Interval = 1000;
+            this.startTimer.Tick += this.StartTimer_Tick;
+            this.startTimer.Start();
+
+            if (Process.GetProcessesByName(procName).Length < 1)
+            {
+                this.StartProgram();
+                this.SetState(State.Ready);
+            }
+            else
+            {
+                this.SetState(State.Inactive);
+            }
+
+            this.TryStartUpdate();
+        }
+
+        private void SetState(State s)
+        {
+            Bitmap bmp = null;
+            switch (s)
+            {
+                case State.Inactive:
+                    bmp = Properties.Resources.status_icon_inactive;
+                    break;
+                case State.Ready:
+                    bmp = Properties.Resources.status_icon_ready;
+                    break;
+                case State.Busy:
+                    bmp = Properties.Resources.status_icon_busy;
+                    break;
+            }
+            this.notifyIcon.Icon = Icon.FromHandle(bmp.GetHicon());
+        }
+
+        private void StopUpdate()
+        {
+            this.updateTimer.Stop();
+            this.startTimer.Start();
+        }
+
+        private void TryStartUpdate()
+        {
+            try
+            {
+                this.processCpu = new PerformanceCounter("Process", "% Processor Time", procName);
+                this.processCpu.NextValue();
+            }
+            catch
+            {
+                return;
+            }
+
+            this.startTimer.Stop();
+            this.updateTimer.Start();
+
+            this.TryUpdate();
+        }
+
+        private void HideWindow()
+        {
+            this.Location = new Point(short.MinValue, short.MinValue);
+        }
+
+        private void RepositionWindow()
+        {
+            var proc = Process.GetProcessesByName(procName)[0];
+            var handle = proc.MainWindowHandle;
+
+            var focus = GetForegroundWindow();
+            if (handle == focus)
+            {
+                RECT r;
+                GetWindowRect(handle, out r);
+                int x = r.Right - this.Width;
+                int y = r.Top;
+                x -= 8;
+                y += 31;
+                x += 1;
+                y -= 1;
+                this.Location = new Point(x, y);
+            }
+            else
+            {
+                this.HideWindow();
+            }
+        }
+
+        private void TryUpdate()
+        {
+            try
+            {
+                var value = this.processCpu.NextValue() / Environment.ProcessorCount;
+                if (value > 5)
+                {
+                    this.BackgroundImage = Properties.Resources.busy_icon;
+                    this.SetState(State.Busy);
+                }
+                else
+                {
+                    this.BackgroundImage = null;
+                    this.SetState(State.Ready);
+                }
+
+                this.RepositionWindow();
+            }
+            catch
+            {
+                this.SetState(State.Inactive);
+                this.HideWindow();
+                this.StopUpdate();
+            }
+        }
+
+        private void StartProgram()
+        {
+            Process.Start(procPath);
+        }
+
+        private void StartTimer_Tick(object sender, EventArgs e)
+        {
+            this.TryStartUpdate();
+        }
+
+        private void UpdateTimer_Tick(object sender, EventArgs e)
+        {
+            this.TryUpdate();
+        }
+
+        private void startViewNXToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.StartProgram();
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
